9bd514a7e46c8ff0041da4166c21b60e4d9df6b4
Pete Katsaboulas <pete@virality.me>
2017-04-06 13:47:11 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+
+    $('.btn_menu').on('click', function(){
+        $(this).toggleClass('active');
+        $('nav, nav ul, #main').toggleClass('active');
+    });
+	
+	
+});
+
+
