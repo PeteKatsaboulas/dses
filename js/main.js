a86483d433332f9e90eae4683eff959414214475
Pete Katsaboulas <pete@virality.me>
2017-04-08 07:36:21 -0500

    new images

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function(){
 
     $('.btn_menu').on('click', function(){
         $(this).toggleClass('active');
-        $('nav, nav ul, #main').toggleClass('active');
+        $('nav, nav ul, #main, body').toggleClass('active');
     });
 	
 	
